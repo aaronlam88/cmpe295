b7e66186ae7be97cca5eb8d40a95b19c8e90f820
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-03-10 18:22:04 -0800

    upgrade databaseManagement

--- src/databaseManagement/DatabaseManager.java
+++ src/databaseManagement/DatabaseManager.java
@@ -1,12 +1,17 @@
 package databaseManagement;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DatabaseManager {
+	private static final Logger LOGGER = Logger.getLogger(DatabaseManager.class.getName());
+
 	LinkedList<String> tablenames = new LinkedList<>();
 	HashMap<String, LinkedList<String>> workerMap = new HashMap<>();
 
@@ -19,8 +24,21 @@ public class DatabaseManager {
 			while(resultSet.next()) {
 				manager.tablenames.add(resultSet.getString(1));
 			}
+			LOGGER.log(Level.INFO, "number of tables: " + manager.tablenames.size() );
+			for(String table : manager.tablenames) {
+				query = "SELECT MAX(Date) FROM `SP500`.`" + table +"`";
+				resultSet = connection.prepareCall(query).executeQuery();
+				if(resultSet.next()) {
+					query = "INSERT INTO `SP500`.`4update` VALUES (?, ?);";
+					PreparedStatement stmt = connection.prepareStatement(query);
+					stmt.setString(1, table);
+					stmt.setDate(2, resultSet.getDate(1));
+					stmt.execute();
+					LOGGER.log(Level.INFO, stmt.toString());
+				}
+			}
+			connection.commit();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
