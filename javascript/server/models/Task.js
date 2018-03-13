42b3bd61527a1a1113713d25b092f383d083ee11
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-03-12 23:25:41 -0700

    get data from database in time range

--- javascript/server/models/Task.js
+++
@@ -1,9 +0,0 @@
-var db = require('../dbconnection'); //reference of dbconnection.js
-
-var Task = {
-  getAllTasks:function(callback){
-    return db.query("Select * from AAL", callback);
-  }
-};
-
-module.exports = Task;
