8ab357b2162e510e64eddb37a8a574093d954df8
“ShuzhongChen” <chenshuzhongs@gmail.com>
2018-03-13 21:36:14 -0700

    remove time

--- javascript/server/models/Stock.js
+++ javascript/server/models/Stock.js
@@ -5,7 +5,7 @@ var Stock = {
     return db.query("Select * from AAL", callback);
   },
   getStockById:function(table,start_date,end_date,callback){
-    let q = "SELECT * FROM " + table + " WHERE Date >= '" + start_date + " 00:00:00' AND Date <= '" + end_date + " 00:00:00'"
+    let q = "SELECT * FROM " + table + " WHERE Date >= '" + start_date + "' AND Date <= '" + end_date + "'"
     console.log("query: ", q);
     return db.query(q, callback);
   },
