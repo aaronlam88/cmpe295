689cf95d1f033807e2b737df97d1fb71f87aa48f
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-03-25 15:17:47 -0700

    init python for ML

---
+++ python/testing/mysql-connection.py
@@ -0,0 +1,28 @@
+# get config to connect to database
+import json
+
+data = json.load(open('../../ignore/db_config.json'))
+
+import mysql.connector
+
+config = {
+  'user': data['username'],
+  'password': data['password'],
+  'host': data['host'],
+  'database': data['database'],
+  'raise_on_warnings': True,
+}
+
+cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
+
+query = ("SELECT * FROM SP500.AAP WHERE Date <=  %s and Date >= %s;")
+start_date =  '2018-02-08' 
+end_date = '2018-02-01'
+
+cursor.execute(query, (start_date, end_date))
+
+for rows in cursor:
+    print(rows)
+
+cnx.close()
