ecabd10e3a95c0bc2d1cdc96c18b7720d814d1c3
aaronlam <aaron.y.lam@sjsu.edu>
2018-04-08 02:40:32 +0000

    set up dev branch

--- javascript/server/app.js
+++ javascript/server/app.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 var Stocks = require('./routes/Stocks');
 var app = express();
 
-var port = 8081;
+var port = 9081;
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
