92e2974649a9925f36535e15b290f8ec9b48873c
Aaron Lam <dungll@uci.edu>
2018-04-21 16:54:46 -0700

    fix cache problem

--- javascript/server/cache/cache.js
+++ javascript/server/cache/cache.js
@@ -1,8 +1,14 @@
 var LRU = require("lru-cache");
-var options = { max: 100
-              , length: function (n, key) { return n * 2 + key.length }
-              , dispose: function (key, n) { n.close() }
-              , maxAge: 1000 * 60 * 60 };
+var options = {
+    max: 100,
+    length: function (n, key) {
+        return n * 2 + key.length
+    },
+    dispose: function (key, n) {
+        n.close()
+    },
+    maxAge: 1000 * 60 * 60
+};
 
 var cache = LRU(options);
 
