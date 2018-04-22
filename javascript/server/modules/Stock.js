4e2824efc2409e8fac463a8c9d91751c8d9b6e49
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-04-21 17:02:17 -0700

    bug fix

--- javascript/server/modules/Stock.js
+++ javascript/server/modules/Stock.js
@@ -3,7 +3,7 @@ var cache = require('../cache/cache.js');
 
 var Stock = {
     getStockById: function (table, start_date, end_date, res) {
-        let q = `SELECT * FROM ${table} WHERE Date >= ${start_date} AND Date <= ${end_date} ORDER BY Date DESC`
+        let q = `SELECT * FROM ${table} WHERE Date >= '${start_date}' AND Date <= '${end_date}' ORDER BY Date DESC`
         console.log("query: ", q);
 
         let cachedResult = cache.get(q)
