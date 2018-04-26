ed92d42ae71632e8758ff5609837a3b62353a31b
AaronLam <aaron.y.lam@sjsu.edu>
2018-04-26 14:35:15 -0700

    reconfig webpack and test

---
+++ javascript/webapp/test/jqueryTest.js
@@ -0,0 +1,14 @@
+var $ = require('jquery');
+
+// $.get(
+//     "http://54.219.174.5:9081/Stocks/AAL/2010-01-02/2018-01-02",
+//     function (data) {
+//         console.log(typeof data);
+//     }
+// ).done(function (data) {
+//     console.log(data);
+// }).fail(function (data) {
+//     console.log('fail');
+// }).always(function (data) {
+//     console.log('always');
+// })
