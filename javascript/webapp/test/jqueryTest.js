056e8cfcfb425fcd70b61b6175971133e102d265
AaronLam <aaron.y.lam@sjsu.edu>
2018-04-26 16:05:57 -0700

    add test

--- javascript/webapp/test/jqueryTest.js
+++ javascript/webapp/test/jqueryTest.js
@@ -1,14 +1,21 @@
-var $ = require('jquery');
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
 
-// $.get(
-//     "http://54.219.174.5:9081/Stocks/AAL/2010-01-02/2018-01-02",
-//     function (data) {
-//         console.log(typeof data);
-//     }
-// ).done(function (data) {
-//     console.log(data);
-// }).fail(function (data) {
-//     console.log('fail');
-// }).always(function (data) {
-//     console.log('always');
-// })
+var $ = require('jquery')(window);
+
+console.log('==== benchmark get api ====');
+console.time('jquery');
+$.get(
+    "http://54.219.174.5:9081/Stocks/AAL/2009-01-02/2018-01-02",
+).done(function (data) {
+    console.log(data.length);
+}).fail(function (data) {
+    console.log('fail');
+}).always(function (data) {
+    console.timeEnd('jquery');
+    console.log('==== end ====');
+});
+console.log('\x1b[31m', 'Note: jquery is async, so this line may print before end msg', '\x1b[0m');
