fc4e00bd872e74142d858f80afe1bc776303821f
“ShuzhongChen” <chenshuzhongs@gmail.com>
2018-10-02 11:18:06 -0700

    reduce training data

--- python/models/Regression_single_stock.py
+++ python/models/Regression_single_stock.py
@@ -16,23 +16,25 @@ accuracy = {}
 features = getData.getSymbolFeaturesWithoutDate('AMZN')
 
 closePrice = []
-
+XOpenClose = []
 for feature in features:
     closePrice.append(feature[3])
+    XOpenClose.append([feature[0],feature[3]])
 
-X = features[0:900]
+
+X = XOpenClose[0:900]
 y = closePrice[1:901]
 
 svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
 svr_lin = SVR(kernel='linear', C=1e3)
 svr_poly = SVR(kernel='poly', C=1e3, degree=2)
 
-y_rbf = svr_rbf.fit(X, y).predict(features[901:999])
+y_rbf = svr_rbf.fit(X, y).predict(XOpenClose[901:999])
 print('y_rbf')
 print(y_rbf)
-y_lin = svr_lin.fit(X, y).predict(features[901:999])
+y_lin = svr_lin.fit(X, y).predict(XOpenClose[901:999])
 print('y_lin')
 print(y_lin)
-y_poly = svr_poly.fit(X, y).predict(features[901:999])
+y_poly = svr_poly.fit(X, y).predict(XOpenClose[901:999])
 print('y_poly')
 print(y_poly)
