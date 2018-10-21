d6ca025a102a9d01afd768ad84828510ee503995
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-10-21 14:29:16 -0700

    update get_data

--- python/unit_test/get_data_test.py
+++ python/unit_test/get_data_test.py
@@ -6,17 +6,22 @@ import unittest
 import csv
 
 class TestGetData(unittest.TestCase):
-    def test_getAllFeatures1(self):
-        getData = GetData()
-        features = getData.getAllFeatures()
-        self.assertIsNotNone(features)
-        self.assertEqual(len(features), 1000)
+    # def test_getAllFeatures1(self):
+    #     getData = GetData()
+    #     features = getData.getAllFeatures()
+    #     self.assertIsNotNone(features)
 
-    def test_getAllFeatures2(self):
-        getData = GetData(101)
-        features = getData.getAllFeatures()
+    # def test_getAllFeatures2(self):
+    #     getData = GetData(101)
+    #     features = getData.getAllFeatures()
+    #     self.assertIsNotNone(features)
+    #     self.assertEqual(len(features), 100)
+    
+    def test_getAllFeatures3(self):
+        getData = GetData(5)
+        features = getData.getAllFeatures('open', 'close')
         self.assertIsNotNone(features)
-        self.assertEqual(len(features), 100)
+        print(features[0])
 
 if __name__ == '__main__':
     unittest.main()
