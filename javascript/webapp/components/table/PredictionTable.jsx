e5dfa91953ac329259d5ed345fdffb103f094447
Qi Liu <liuqi0087@gmail.com>
2018-10-27 15:53:20 -0700

    update json for test and add algorithm 3 & 4 style

--- javascript/webapp/components/table/PredictionTable.jsx
+++ javascript/webapp/components/table/PredictionTable.jsx
@@ -23,7 +23,7 @@ class PredictionTable extends Component {
         axios.get(`http://54.176.230.26:8081/Predict/top`)
             .then(res => {
                 const data = res.data;
-                this.setState({ data });
+                this.setState({data});
             })
     }
 
@@ -74,8 +74,7 @@ class PredictionTable extends Component {
                 // background: v.value === '0%' ? 'rgba(96, 239, 255, 0.4)' : 'red',
                 color: 'black',
             },
-        }
-        ];
+        }];
 
         // column style for loser
         const loserColumn = [{
@@ -91,8 +90,31 @@ class PredictionTable extends Component {
                 // background: v.value === '0%' ? 'rgba(96, 239, 255, 0.4)' : 'red',
                 color: 'black',
             },
-        }
-        ];
+        }];
+
+        // column style for prediction goes higher
+        const higherColumn = [{
+            accessor: 'result',
+            style: {
+                background: 'rgba(96, 239, 255, 0.8)',
+                // background: v.value === '0%' ? 'rgba(96, 239, 255, 0.4)' : 'red',
+                color: 'black',
+            },
+        }];
+
+        // column style for prediction goes lower
+        const lowerColumn = [{
+            accessor: 'result',
+            style: {
+                background: 'rgba(255, 0, 167, 0.8)',
+                // background: v.value === '0%' ? 'rgba(96, 239, 255, 0.4)' : 'red',
+                color: 'black',
+            },
+        }];
+
+        let testData = [{
+            result: 1,
+        }];
 
         return (
             <Grid fluid>
@@ -121,14 +143,16 @@ class PredictionTable extends Component {
                             showPaginationBottom={false}
                         />
                     </Col>
-                    <Col sm={12} md={12} lg={12}><hr /></Col>
+                    <Col sm={12} md={12} lg={12}>
+                        <hr/>
+                    </Col>
 
                     {/*algorithm 2*/}
                     <Col sm={12} md={12} lg={12}>
                         <h2 className={"centerText bottomPadding20"}>Algorithm 2 - xxx Prediction Result</h2>
                     </Col>
                     <Col sm={12} md={12} lg={6} className="pre_loser">
-                        <h3 className={"purpleColor"}>Top 5 Losers -  Algorithm 1</h3>
+                        <h3 className={"purpleColor"}>Top 5 Losers - Algorithm 1</h3>
                         <ReactTable
                             data={loser1}
                             noDataText='Loading Data ...'
@@ -137,6 +161,33 @@ class PredictionTable extends Component {
                             showPaginationBottom={false}
                         />
                     </Col>
+                    <Col sm={12} md={12} lg={12}>
+                        <hr/>
+                    </Col>
+
+                    {/*algorithm 3 and 4 */}
+                    <Col sm={12} md={12} lg={6} className="pre_winner">
+                        <h3 className={"centerText"}>Algorithm 3 prediction Result</h3>
+                        <ReactTable
+                            data={testData}
+                            noDataText='Loading Data ...'
+                            columns={higherColumn}
+                            defaultPageSize={1}
+                            showPaginationBottom={false}
+                        />
+                    </Col>
+
+                    <Col sm={12} md={12} lg={6} className="pre_loser">
+                        <h3 className={"centerText"}>Algorithm 4 prediction Result</h3>
+                        <ReactTable
+                            data={testData}
+                            noDataText='Loading Data ...'
+                            columns={lowerColumn}
+                            defaultPageSize={1}
+                            showPaginationBottom={false}
+                        />
+                    </Col>
+
                 </Row>
             </Grid>
         )
