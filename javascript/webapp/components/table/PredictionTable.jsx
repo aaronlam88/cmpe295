c0bb6026d297d0f22d822460e6c4b71c4aad7a73
Qi Liu <liuqi0087@gmail.com>
2018-10-26 22:52:08 -0700

    update gainer and loser fake data for testing and update style

--- javascript/webapp/components/table/PredictionTable.jsx
+++ javascript/webapp/components/table/PredictionTable.jsx
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import ReactTable from "react-table";
+import {
+    Grid,
+    Row,
+    Col,
+} from 'react-bootstrap';
 
 // import css for react-table
 import './Table.scss';
 
-const data = [{
+const data11 = [{
     company: 'Company AAA',
     number: 5.65,
     percentage: '+1.7%',
@@ -23,7 +28,29 @@ const data = [{
 }, {
     company: 'Company EEE',
     number: 1.22,
-    percentage: '+0.15%',
+    percentage: '0%',
+}];
+
+const data12 = [{
+    company: 'Company UUU',
+    number: 4.97,
+    percentage: '-1.3%',
+}, {
+    company: 'Company WWW',
+    number: 4.01,
+    percentage: '-1.15%',
+}, {
+    company: 'Company XXX',
+    number: 3.65,
+    percentage: '-0.72%',
+}, {
+    company: 'Company YYY',
+    number: 2.49,
+    percentage: '-0.68%',
+}, {
+    company: 'Company ZZZ',
+    number: 1.92,
+    percentage: '-0.42%',
 }];
 
 class PredictionTable extends Component {
@@ -33,36 +60,68 @@ class PredictionTable extends Component {
 
         // create state from props that pass down from parent
         this.state = {
-            data: data,
+            data11: data11,
+            data12: data12,
         };
     }
 
     render() {
-        const columns = [{
-            // Header: 'Name',
+        // let currPer = this.state.data => percentage;
+        const columns1 = [{
+            // Header: 'Company',
             accessor: 'company'
         }, {
-            // Header: 'Age',
+            // Header: 'Number',
             accessor: 'number'
         }, {
             accessor: 'percentage',
             style: {
-                background: 'rgba(96, 239, 255, 0.4)',
+                background: 'rgba(96, 239, 255, 0.5)',
+                // background: v.value === '0%' ? 'rgba(96, 239, 255, 0.4)' : 'red',
                 color: 'black',
+            },
+        }
+        ];
 
+        const columns2 = [{
+            // Header: 'Company',
+            accessor: 'company'
+        }, {
+            // Header: 'Number',
+            accessor: 'number'
+        }, {
+            accessor: 'percentage',
+            style: {
+                background: 'rgba(255, 0, 167, 0.5)',
+                // background: v.value === '0%' ? 'rgba(96, 239, 255, 0.4)' : 'red',
+                color: 'black',
             },
         }
         ];
 
         return (
-            <div id="pre_winner">
-                <ReactTable
-                    data={data}
-                    columns={columns}
-                    defaultPageSize={5}
-                    showPaginationBottom={false}
-                />
-            </div>
+            <Grid fluid>
+                <Row>
+                    <Col sm={12} md={12} lg={6} className="pre_winner">
+                        <h2 className={"blueColor"}>Top 5 Gainers - Algorithm 1</h2>
+                        <ReactTable
+                            data={data11}
+                            columns={columns1}
+                            defaultPageSize={5}
+                            showPaginationBottom={false}
+                        />
+                    </Col>
+                    <Col sm={12} md={12} lg={6} className="pre_loser">
+                        <h2 className={"purpleColor"}>Top 5 Losers -  Algorithm 1</h2>
+                        <ReactTable
+                            data={data12}
+                            columns={columns2}
+                            defaultPageSize={5}
+                            showPaginationBottom={false}
+                        />
+                    </Col>
+                </Row>
+            </Grid>
         )
 
     }
