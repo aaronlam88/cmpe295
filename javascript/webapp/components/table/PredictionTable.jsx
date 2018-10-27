8c6c2d47586a232ee84a819adfc6e5952d912cff
Qi Liu <liuqi0087@gmail.com>
2018-10-27 02:20:13 -0700

    Install axios and now can successfully read Jason data from url

--- javascript/webapp/components/table/PredictionTable.jsx
+++ javascript/webapp/components/table/PredictionTable.jsx
@@ -5,6 +5,7 @@ import {
     Row,
     Col,
 } from 'react-bootstrap';
+import axios from 'axios';
 
 // import css for react-table
 import './Table.scss';
@@ -57,96 +58,126 @@ class PredictionTable extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            data: [],
+            company: [],
+            percentage: [],
+        }
 
         // create state from props that pass down from parent
-        this.state = {
-            data11: data11,
-            data12: data12,
-        };
+        // this.state = {
+        //     data11: data11,
+        //     data12: data12,
+        // };
+    }
+
+    componentDidMount() {
+        axios.get(`http://54.176.230.26:8081/Predict/top`)
+            .then(res => {
+                const data = res.data;
+                this.setState({ data });
+            })
     }
 
     render() {
-        // let currPer = this.state.data => percentage;
-        const columns1 = [{
-            // Header: 'Company',
-            accessor: 'company'
-        }, {
-            // Header: 'Number',
-            accessor: 'number'
-        }, {
-            accessor: 'percentage',
-            style: {
-                background: 'rgba(96, 239, 255, 0.5)',
-                // background: v.value === '0%' ? 'rgba(96, 239, 255, 0.4)' : 'red',
-                color: 'black',
-            },
-        }
-        ];
-
-        const columns2 = [{
-            // Header: 'Company',
-            accessor: 'company'
-        }, {
-            // Header: 'Number',
-            accessor: 'number'
-        }, {
-            accessor: 'percentage',
-            style: {
-                background: 'rgba(255, 0, 167, 0.5)',
-                // background: v.value === '0%' ? 'rgba(96, 239, 255, 0.4)' : 'red',
-                color: 'black',
-            },
-        }
-        ];
-
         return (
-            <Grid fluid>
-                <Row>
-                    {/*algorithm 1*/}
-                    <Col sm={12} md={12} lg={12}>
-                        <h2 className={"centerText bottomPadding20"}>Algorithm 1 - Regression Prediction Result</h2>
-                    </Col>
-                    <Col sm={12} md={12} lg={6} className="pre_winner">
-                        <h3 className={"blueColor centerText"}>Top 5 Gainers</h3>
-                        <ReactTable
-                            data={data11}
-                            noDataText='Loading Data ...'
-                            columns={columns1}
-                            defaultPageSize={5}
-                            showPaginationBottom={false}
-                        />
-                    </Col>
-                    <Col sm={12} md={12} lg={6} className="pre_loser">
-                        <h3 className={"purpleColor centerText"}>Top 5 Losers</h3>
-                        <ReactTable
-                            data={data12}
-                            noDataText='Loading Data ...'
-                            columns={columns2}
-                            defaultPageSize={5}
-                            showPaginationBottom={false}
-                        />
-                    </Col>
-                    <Col sm={12} md={12} lg={12}><hr /></Col>
+            <table>
+                <tbody>{this.state.data.map(function(item, key) {
 
-                    {/*algorithm 2*/}
-                    <Col sm={12} md={12} lg={12}>
-                        <h2 className={"centerText bottomPadding20"}>Algorithm 2 - xxx Prediction Result</h2>
-                    </Col>
-                    <Col sm={12} md={12} lg={6} className="pre_loser">
-                        <h3 className={"purpleColor"}>Top 5 Losers -  Algorithm 1</h3>
-                        <ReactTable
-                            data={data12}
-                            noDataText='Loading Data ...'
-                            columns={columns2}
-                            defaultPageSize={5}
-                            showPaginationBottom={false}
-                        />
-                    </Col>
-                </Row>
-            </Grid>
+                    return (
+                        <tr key = {key}>
+                            <td>{item.label}</td>
+                            <td>{item.result}</td>
+                        </tr>
+                    )
+
+                })}</tbody>
+            </table>
         )
-
     }
+
+    // render() {
+    //     // let currPer = this.state.data => percentage;
+    //     const columns1 = [{
+    //         // Header: 'Company',
+    //         accessor: 'company'
+    //     }, {
+    //         // Header: 'Number',
+    //         accessor: 'number'
+    //     }, {
+    //         accessor: 'percentage',
+    //         style: {
+    //             background: 'rgba(96, 239, 255, 0.5)',
+    //             // background: v.value === '0%' ? 'rgba(96, 239, 255, 0.4)' : 'red',
+    //             color: 'black',
+    //         },
+    //     }
+    //     ];
+    //
+    //     const columns2 = [{
+    //         // Header: 'Company',
+    //         accessor: 'company'
+    //     }, {
+    //         // Header: 'Number',
+    //         accessor: 'number'
+    //     }, {
+    //         accessor: 'percentage',
+    //         style: {
+    //             background: 'rgba(255, 0, 167, 0.5)',
+    //             // background: v.value === '0%' ? 'rgba(96, 239, 255, 0.4)' : 'red',
+    //             color: 'black',
+    //         },
+    //     }
+    //     ];
+    //
+    //     return (
+    //         <Grid fluid>
+    //             <Row>
+    //                 {/*algorithm 1*/}
+    //                 <Col sm={12} md={12} lg={12}>
+    //                     <h2 className={"centerText bottomPadding20"}>Algorithm 1 - Regression Prediction Result</h2>
+    //                 </Col>
+    //                 <Col sm={12} md={12} lg={6} className="pre_winner">
+    //                     <h3 className={"blueColor centerText"}>Top 5 Gainers</h3>
+    //                     <ReactTable
+    //                         data={data11}
+    //                         noDataText='Loading Data ...'
+    //                         columns={columns1}
+    //                         defaultPageSize={5}
+    //                         showPaginationBottom={false}
+    //                     />
+    //                 </Col>
+    //                 <Col sm={12} md={12} lg={6} className="pre_loser">
+    //                     <h3 className={"purpleColor centerText"}>Top 5 Losers</h3>
+    //                     <ReactTable
+    //                         data={data12}
+    //                         noDataText='Loading Data ...'
+    //                         columns={columns2}
+    //                         defaultPageSize={5}
+    //                         showPaginationBottom={false}
+    //                     />
+    //                 </Col>
+    //                 <Col sm={12} md={12} lg={12}><hr /></Col>
+    //
+    //                 {/*algorithm 2*/}
+    //                 <Col sm={12} md={12} lg={12}>
+    //                     <h2 className={"centerText bottomPadding20"}>Algorithm 2 - xxx Prediction Result</h2>
+    //                 </Col>
+    //                 <Col sm={12} md={12} lg={6} className="pre_loser">
+    //                     <h3 className={"purpleColor"}>Top 5 Losers -  Algorithm 1</h3>
+    //                     <ReactTable
+    //                         data={data12}
+    //                         noDataText='Loading Data ...'
+    //                         columns={columns2}
+    //                         defaultPageSize={5}
+    //                         showPaginationBottom={false}
+    //                     />
+    //                 </Col>
+    //             </Row>
+    //         </Grid>
+    //     )
+    //
+    // }
 }
 
 export default PredictionTable;
