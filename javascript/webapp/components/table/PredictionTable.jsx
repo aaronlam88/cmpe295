8ae37e2d7c8d89ebc2be259830856d4a9a8c4626
Qi Liu <liuqi0087@gmail.com>
2018-10-27 17:21:07 -0700

    Add functions to separate json data

--- javascript/webapp/components/table/PredictionTable.jsx
+++ javascript/webapp/components/table/PredictionTable.jsx
@@ -16,50 +16,82 @@ class PredictionTable extends Component {
         super(props);
         this.state = {
             data: [],
-        }
+        };
     }
 
     componentDidMount() {
         axios.get(`http://54.176.230.26:8081/Predict/top`)
             .then(res => {
-                const data = res.data;
-                this.setState({data});
-            })
+                this.setState({data: res.data});
+            });
     }
 
-    processData() {
-        let allJason = this.state.data.map(
+    //gainer in Algorithm 1
+    alg01Gainer() {
+        return (this.state.data.slice(0, 5).map(
             item => ({
                 company: item.label,
                 amount: item.amount,
-                percentage: item.result,
+                percentage: item.result + '%',
             })
-        );
-        console.log(allJason)
+        ))
     }
+
+    // loser in Algorithm 1
+    alg01Loser() {
+        return (this.state.data.slice(5, 10).map(
+            item => ({
+                company: item.label,
+                amount: item.amount,
+                percentage: item.result + '%',
+            })
+        ))
+    }
+
+    //gainer in Algorithm 2
+    alg02Gainer() {
+        return (this.state.data.slice(10, 15).map(
+            item => ({
+                company: item.label,
+                amount: item.amount,
+                percentage: item.result + '%',
+            })
+        ))
+    }
+
+
+    // loser in Algorithm 2
+    alg02Loser() {
+        return (this.state.data.slice(15, 20).map(
+            item => ({
+                company: item.label,
+                amount: item.amount,
+                percentage: item.result + '%',
+            })
+        ))
+    }
+
+
+    // algorithm 3 result
+    alg03Result() {
+        return (this.state.data.slice(20, 21).map(
+            item => ({
+                result: item.result,
+            })
+        ))
+    }
+
+    //algorithm 4 result
+    alg04Result() {
+        return (this.state.data.slice(21, 22).map(
+            item => ({
+                result: item.result,
+            })
+        ))
+    }
+
 
     render() {
-        this.processData();
-
-        //gainer in Algorithm 1
-        let size = 5;
-        let gainer1 = this.state.data.slice(0, size).map(
-            item => ({
-                company: item.label,
-                amount: item.amount,
-                percentage: item.result,
-            })
-        );
-
-        // loser in Algorithm 1
-        let loser1 = this.state.data.slice(5, 10).map(
-            item => ({
-                company: item.label,
-                amount: item.amount,
-                percentage: item.result,
-            })
-        );
-
         // column style for gainer
         const gainerColumn = [{
             // Header: 'Company',
@@ -112,10 +144,6 @@ class PredictionTable extends Component {
             },
         }];
 
-        let testData = [{
-            result: 1,
-        }];
-
         return (
             <Grid fluid>
                 <Row>
@@ -126,7 +154,7 @@ class PredictionTable extends Component {
                     <Col sm={12} md={12} lg={6} className="pre_winner">
                         <h3 className={"blueColor centerText"}>Top 5 Gainers</h3>
                         <ReactTable
-                            data={gainer1}
+                            data={this.alg01Gainer()}
                             noDataText='Loading Data ...'
                             columns={gainerColumn}
                             defaultPageSize={5}
@@ -136,7 +164,7 @@ class PredictionTable extends Component {
                     <Col sm={12} md={12} lg={6} className="pre_loser">
                         <h3 className={"purpleColor centerText"}>Top 5 Losers</h3>
                         <ReactTable
-                            data={loser1}
+                            data={this.alg01Loser()}
                             noDataText='Loading Data ...'
                             columns={loserColumn}
                             defaultPageSize={5}
@@ -151,10 +179,20 @@ class PredictionTable extends Component {
                     <Col sm={12} md={12} lg={12}>
                         <h2 className={"centerText bottomPadding20"}>Algorithm 2 - xxx Prediction Result</h2>
                     </Col>
-                    <Col sm={12} md={12} lg={6} className="pre_loser">
-                        <h3 className={"purpleColor"}>Top 5 Losers - Algorithm 1</h3>
+                    <Col sm={12} md={12} lg={6} className="pre_winner">
+                        <h3 className={"blueColor centerText"}>Top 5 Gainers</h3>
                         <ReactTable
-                            data={loser1}
+                            data={this.alg02Gainer()}
+                            noDataText='Loading Data ...'
+                            columns={gainerColumn}
+                            defaultPageSize={5}
+                            showPaginationBottom={false}
+                        />
+                    </Col>
+                    <Col sm={12} md={12} lg={6} className="pre_loser">
+                        <h3 className={"purpleColor centerText"}>Top 5 Losers</h3>
+                        <ReactTable
+                            data={this.alg02Loser()}
                             noDataText='Loading Data ...'
                             columns={loserColumn}
                             defaultPageSize={5}
@@ -167,9 +205,9 @@ class PredictionTable extends Component {
 
                     {/*algorithm 3 and 4 */}
                     <Col sm={12} md={12} lg={6} className="pre_winner">
-                        <h3 className={"centerText"}>Algorithm 3 prediction Result</h3>
+                        <h2 className={"centerText"}>Algorithm 3 prediction Result</h2>
                         <ReactTable
-                            data={testData}
+                            data={this.alg03Result()}
                             noDataText='Loading Data ...'
                             columns={higherColumn}
                             defaultPageSize={1}
@@ -178,9 +216,9 @@ class PredictionTable extends Component {
                     </Col>
 
                     <Col sm={12} md={12} lg={6} className="pre_loser">
-                        <h3 className={"centerText"}>Algorithm 4 prediction Result</h3>
+                        <h2 className={"centerText"}>Algorithm 4 prediction Result</h2>
                         <ReactTable
-                            data={testData}
+                            data={this.alg04Result()}
                             noDataText='Loading Data ...'
                             columns={lowerColumn}
                             defaultPageSize={1}
