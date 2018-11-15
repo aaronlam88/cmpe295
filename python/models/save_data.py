8c9108a6fd317f2f0c017f82d56d734551d4b468
Shuzhong Chen <chenshuzhongs@gmail.com>
2018-11-15 14:53:38 -0800

    delete the surplus code (#148)

--- python/models/save_data.py
+++ python/models/save_data.py
@@ -15,38 +15,6 @@ logger = logging.getLogger(__name__)
 class SaveData:
     """This class provide functions to save data to our prediction database"""
 
-    # def saveData(self, table, date, value):
-    #     try:
-    #         logger.debug('Saving data to database')
-    #         config = {
-    #             'user': "cmpe295",
-    #             'password': "cmpe295.sjsu.2018",
-    #             'host': "stockdatabase.cxswepygqy9j.us-west-1.rds.amazonaws.com",
-    #             'database': "PredictionDatabase",
-    #             'raise_on_warnings': True,
-    #             'buffered': True
-    #         }
-    #         cnx = mysql.connector.connect(**config)
-    #         cursor = cnx.cursor()
-    #
-    #         # data column |Date|Value|
-    #
-    #         # check if table exist. if not, create a new one
-    #         if not self.checkTableExists(cnx, table):
-    #             logger.debug("""Create a new table %s""" % (table))
-    #             query = """CREATE TABLE IF NOT EXISTS `%s` (`Date` DATETIME NOT NULL, `Value` DOUBLE NULL, PRIMARY KEY (`Date`));""" % (table)
-    #             cursor.execute(query)
-    #
-    #         # insert the data to the table
-    #         query = """INSERT INTO `%s` (Date, Value) VALUES (%s, %s);""" % (table, date, value)
-    #         print(query)
-    #         cursor.execute(query)
-    #         cnx.commit()
-    #         cursor.close()
-    #     except Exception:
-    #         logger.error('Unable to fecth data: ', exc_info=True)
-
-
     def saveMultipleData(self, table, algo, records_to_insert):
         try:
             logger.debug('Saving data to database')
